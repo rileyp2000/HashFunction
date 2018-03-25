50a3e28745334f5ca0c9ccd639d3f81244cd79fa
rileyp2000 <rileyp@bishopireton.org>
2018-03-24 21:53:01 -0400

    finished part 1 i hope

--- HashFunctions/src/TicTacToeHashCode.java
+++ HashFunctions/src/TicTacToeHashCode.java
@@ -6,103 +6,89 @@ import java.util.Scanner;
 
 public class TicTacToeHashCode extends Board {
 
- boolean [] winners;  // True if the hash string that maps to this index is a winner, false otherwise
-    
-  TicTacToeHashCode(String s) {
-   super(s);
-  // TODO Instantiate/fill winners array.  
-   winners = new boolean[(int)Math.pow(3, 9)];
-   fillWinners();
-   //read in file of winners
-   //then calc hash of each
-   //place in array
-  }
-  
-  public void fillWinners() {
-	  /*System.out.print("Enter the Winners File Name: ");
-	  Scanner kybd = new Scanner(System.in);
-	  String fn = kybd.nextLine();
-	  kybd.close();*/
-	  
-	  String fn = "TicTacToeWinners.txt";
-	  
-	  
-	  Scanner file = fileToScanner(fn);
-	  
-	  while(file.hasNextLine()) {
-		  String board = file.nextLine();
-		  super.setBoardString(board);
-		  winners[myHashCode()] = true;
-	  }
-		  
-  }
-  
-  public Scanner fileToScanner(String s) {
-	  File f = new File(s);
-	  Scanner ret = null;
-	  
-	  try {
-		  ret = new Scanner(f);
-	  }catch(FileNotFoundException e) {
-		  System.out.println("No File!!");
-		  System.exit(1);
-	  }
-	  
-	  return ret;
-  }
+	boolean[] winners; // True if the hash string that maps to this index is a
+						// winner, false otherwise
 
-  @Override
-    public int myHashCode() {
-	  int[][] pow3 = new int[][] {{1,3,9},{27,81,243},{729,2187,6561}};
-	  int total = 0;
-	  int curChar = 0;
-	  
-	  for(int r = 0; r < 3; r ++) {
-		  for(int c = 0; c < 3; c++) {
-			  char item = super.charAt(r, c);
-			  switch(item) {
-			  case 'x':
-				  curChar = 1;
-				  break;
-			  case 'o':
-				  curChar = 2;
-				  break;
-			  default:
-				  curChar = 0;
-				  
-			  }
-			  total += curChar * pow3[r][c];
-		  }
-	  }
-	  
-	  return total;	  
-   
-  }
-   
-    public boolean isWin(String s) {
-    	super.setBoardString(s);
-    	return winners[myHashCode()];
-   
-    }
-    
-    public boolean isWin() {
-    	return winners[myHashCode()];
-    }
-  
-   public static void main(String[] args) throws InterruptedException {
-      TicTacToeHashCode board = new TicTacToeHashCode ("Tic Tac Toe");
-     while (true) {
-      
-       //TODO this line no longer works
-       //  String currentBoard = board.boardValues[(int)(Math.random()* board.boardValues.length)];
-         
-         board.displayRandomString();
-         board.setHashCodeLabel(board.myHashCode());
-         // TODO Update this line to call your isWin method.
-         board.setWinnerLabel(board.isWin(board.getBoardString()));
-         
-         Thread.sleep(1000);      
-      }
-   }
- }  
-  
+	TicTacToeHashCode(String s) {
+		super(s);
+		winners = new boolean[(int)Math.pow(3, 9)];
+		fillWinners();
+		}
+
+	public void fillWinners() {
+
+		String fn = "TicTacToeWinners.txt";
+
+		Scanner file = fileToScanner(fn);
+
+		while (file.hasNextLine()) {
+			String board = file.nextLine();
+			super.setBoardString(board);
+			winners[myHashCode()] = true;
+		}
+
+	}
+
+	public Scanner fileToScanner(String s) {
+		File f = new File(s);
+		Scanner ret = null;
+
+		try {
+			ret = new Scanner(f);
+		} catch (FileNotFoundException e) {
+			System.out.println("No File!!");
+			System.exit(1);
+		}
+
+		return ret;
+	}
+
+	@Override
+	public int myHashCode() {
+		int[][] pow3 = new int[][] { { 1, 3, 9 }, { 27, 81, 243 }, { 729, 2187, 6561 } };
+		int total = 0;
+		int curChar = 0;
+
+		for (int r = 0; r < 3; r++) {
+			for (int c = 0; c < 3; c++) {
+				char item = super.charAt(r, c);
+				switch (item) {
+				case 'x':
+					curChar = 1;
+					break;
+				case 'o':
+					curChar = 2;
+					break;
+				default:
+					curChar = 0;
+
+				}
+				total += curChar * pow3[r][c];
+			}
+		}
+
+		return total;
+
+	}
+
+	@Override
+	public boolean isWin(String s) {
+		super.setBoardString(s);
+		return winners[myHashCode()];
+
+	}
+
+	public boolean isWin() {
+		return winners[myHashCode()];
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		TicTacToeHashCode board = new TicTacToeHashCode("Tic Tac Toe");
+		while (true) {
+			board.displayRandomString();
+			System.out.println("This should be a winner: " + board.isWin());
+			Thread.sleep(4000);
+		}
+	}
+
+}
