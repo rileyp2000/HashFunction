dbfc21b130cb9093bf4ac359b58816179d723f02
rileyp2000 <rileyp@bishopireton.org>
2018-03-24 22:05:13 -0400

    new class for pt 2

---
+++ HashFunctions/src/TTT_HC.java
@@ -0,0 +1,34 @@
+
+
+public class TTT_HC extends Board{
+	
+	private boolean winners;
+	
+	public TTT_HC(String title) {
+		super(title);
+		fillWinners();
+	}
+	
+	private void fillWinners(){
+		
+	}
+
+	@Override
+	int myHashCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	boolean isWin(String s) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	boolean isWin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
