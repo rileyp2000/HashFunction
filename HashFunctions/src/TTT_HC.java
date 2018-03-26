36f2cce44554f1691932f17285ee175f982907b3
rileyp2000 <rileyp@bishopireton.org>
2018-03-26 00:31:42 -0400

    working on it

--- HashFunctions/src/TTT_HC.java
+++ HashFunctions/src/TTT_HC.java
@@ -1,22 +1,40 @@
+import java.util.HashMap;
 
+public class TTT_HC extends Board {
 
-public class TTT_HC extends Board{
-	
-	private boolean winners;
-	
+	private HashMap<String, Object> winners;
+
 	public TTT_HC(String title) {
 		super(title);
 		fillWinners();
 	}
-	
-	private void fillWinners(){
-		
+
+	private void fillWinners() {
+
 	}
 
 	@Override
 	int myHashCode() {
-		// TODO Auto-generated method stub
-		return 0;
+		int total = 0;
+		int curChar = 0;
+		for (int r = 0; r < 3; r++) {
+			for (int c = 0; c < 3; c++) {
+				char item = super.charAt(r, c);
+				switch (item) {
+				case 'x':
+					curChar = 2;
+					break;
+				case 'o':
+					curChar = 1;
+					break;
+				default:
+					curChar = 0;
+
+				}
+				total += curChar * ((r + 1) * (c + 1));
+			}
+		}
+		return total;
 	}
 
 	@Override
